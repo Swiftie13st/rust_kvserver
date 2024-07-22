93244e00eea3d907399dc2180ebd39819c69e408
Bruce <swiftie13s@163.com>
2024-07-22 16:11:51 +0800

    sleddb

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,24 +1,15 @@
-/*
-    引入 abi.rs，并做一些基本的类型转换
-*/
 pub mod abi;
 
-use crate::KvError;
+use std::convert::TryFrom;
+
 use abi::{command_request::RequestData, *};
+use bytes::Bytes;
 use http::StatusCode;
+use prost::Message;
+
+use crate::KvError;
 
 impl CommandRequest {
-    /// 创建 HSET 命令
-    pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
-        Self {
-            request_data: Some(RequestData::Hset(Hset {
-                table: table.into(),
-                pair: Some(Kvpair::new(key, value)),
-            })),
-        }
-    }
-
-    /// 创建 HGET 命令
     pub fn new_hget(table: impl Into<String>, key: impl Into<String>) -> Self {
         Self {
             request_data: Some(RequestData::Hget(Hget {
@@ -28,7 +19,6 @@ impl CommandRequest {
         }
     }
 
-    /// 创建 HGETALL 命令
     pub fn new_hgetall(table: impl Into<String>) -> Self {
         Self {
             request_data: Some(RequestData::Hgetall(Hgetall {
@@ -37,27 +27,33 @@ impl CommandRequest {
         }
     }
 
-    /// 创建 HMGET 命令
     pub fn new_hmget(table: impl Into<String>, keys: Vec<String>) -> Self {
         Self {
             request_data: Some(RequestData::Hmget(Hmget {
                 table: table.into(),
-                keys: keys,
+                keys,
             })),
         }
     }
 
-    /// 创建 HMSET 命令
+    pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
+        Self {
+            request_data: Some(RequestData::Hset(Hset {
+                table: table.into(),
+                pair: Some(Kvpair::new(key, value)),
+            })),
+        }
+    }
+
     pub fn new_hmset(table: impl Into<String>, pairs: Vec<Kvpair>) -> Self {
         Self {
             request_data: Some(RequestData::Hmset(Hmset {
                 table: table.into(),
-                pairs: pairs,
+                pairs,
             })),
         }
     }
 
-    /// 创建 HDEL 命令
     pub fn new_hdel(table: impl Into<String>, key: impl Into<String>) -> Self {
         Self {
             request_data: Some(RequestData::Hdel(Hdel {
@@ -67,7 +63,6 @@ impl CommandRequest {
         }
     }
 
-    /// 创建 HMDEL 命令
     pub fn new_hmdel(table: impl Into<String>, keys: Vec<String>) -> Self {
         Self {
             request_data: Some(RequestData::Hmdel(Hmdel {
@@ -77,7 +72,6 @@ impl CommandRequest {
         }
     }
 
-    /// 创建 HEXIST 命令
     pub fn new_hexist(table: impl Into<String>, key: impl Into<String>) -> Self {
         Self {
             request_data: Some(RequestData::Hexist(Hexist {
@@ -87,7 +81,6 @@ impl CommandRequest {
         }
     }
 
-    /// 创建 HMEXIST 命令
     pub fn new_hmexist(table: impl Into<String>, keys: Vec<String>) -> Self {
         Self {
             request_data: Some(RequestData::Hmexist(Hmexist {
@@ -177,7 +170,6 @@ impl From<KvError> for CommandResponse {
     }
 }
 
-/// 从 Vec<Value> 转换成 CommandResponse
 impl From<Vec<Value>> for CommandResponse {
     fn from(v: Vec<Value>) -> Self {
         Self {
@@ -188,11 +180,80 @@ impl From<Vec<Value>> for CommandResponse {
     }
 }
 
-/// 从 Vec<Value> 转换成 CommandResponse
 impl From<bool> for Value {
-    fn from(v: bool) -> Self {
+    fn from(b: bool) -> Self {
         Self {
-            value: Some(value::Value::Bool(v)),
+            value: Some(value::Value::Bool(b)),
         }
+    }
+}
+
+impl From<f64> for Value {
+    fn from(f: f64) -> Self {
+        Self {
+            value: Some(value::Value::Float(f)),
+        }
+    }
+}
+
+impl TryFrom<Value> for i64 {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Integer(i)) => Ok(i),
+            _ => Err(KvError::ConvertError(v, "Integer")),
+        }
+    }
+}
+
+impl TryFrom<Value> for f64 {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Float(f)) => Ok(f),
+            _ => Err(KvError::ConvertError(v, "Float")),
+        }
+    }
+}
+
+impl TryFrom<Value> for Bytes {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Binary(b)) => Ok(b),
+            _ => Err(KvError::ConvertError(v, "Binary")),
+        }
+    }
+}
+
+impl TryFrom<Value> for bool {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Bool(b)) => Ok(b),
+            _ => Err(KvError::ConvertError(v, "Boolean")),
+        }
+    }
+}
+
+impl TryFrom<Value> for Vec<u8> {
+    type Error = KvError;
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        let mut buf = Vec::with_capacity(v.encoded_len());
+        v.encode(&mut buf)?;
+        Ok(buf)
+    }
+}
+
+impl TryFrom<&[u8]> for Value {
+    type Error = KvError;
+
+    fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
+        let msg = Value::decode(data)?;
+        Ok(msg)
     }
 }
